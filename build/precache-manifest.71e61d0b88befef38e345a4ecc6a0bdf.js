20c4a81740e9dc2b64be348685167dfa39b95e20
Ashish Patel <ashishp@vmware.com>
2020-08-08 13:35:07 -0700

    build included

---
+++ build/precache-manifest.71e61d0b88befef38e345a4ecc6a0bdf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca460711143989e45b5b9d70fdcc80bb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc29deac2010aa8a4110",
+    "url": "/static/css/2.fea45d51.chunk.css"
+  },
+  {
+    "revision": "28274285b37fddeff8d4",
+    "url": "/static/css/main.0d9c879a.chunk.css"
+  },
+  {
+    "revision": "dc29deac2010aa8a4110",
+    "url": "/static/js/2.861609c1.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/static/js/2.861609c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28274285b37fddeff8d4",
+    "url": "/static/js/main.ace8c421.chunk.js"
+  },
+  {
+    "revision": "9ee52af2b1aeb7141733",
+    "url": "/static/js/runtime-main.990cc3fe.js"
+  }
+]);
